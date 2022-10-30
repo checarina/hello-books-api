874c7efadebe4c93234f8d6a9e05e94113e3131d
checarina <checarina@gmail.com>
2022-10-29 19:35:57 -0700

    implemented routes for 400 error codes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -39,6 +39,7 @@ def broken_endpoint():
     response_body["hobbies"].append(new_hobby) #fixed
     return response_body
 
+
 @books_bp.route("", methods = ["GET"])
 def handle_books():
     books_response = []
@@ -49,3 +50,25 @@ def handle_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+@books_bp.route("/<book_id>", methods = ["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+        }
+    
