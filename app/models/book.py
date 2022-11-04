d718259258a7b93672ef40e10526ece9c75e6c7b
Carina <checarina@gmail.com>
2022-11-04 16:25:57 -0700

    implemented from_dict Book class method

--- app/models/book.py
+++ app/models/book.py
@@ -7,9 +7,16 @@ class Book(db.Model):
 
     def to_dict(self):
         book_dict = {}
-        
+
         book_dict["id"] = self.id
         book_dict["title"] = self.title
         book_dict["description"] = self.description
 
         return book_dict
+    
+    @classmethod
+    def from_dict(cls, book_data):
+        #Initialize an instance of Book with the title and description from book_data
+        #and with an id generated as usual for the Book class
+        #return the Book instance
+        return Book(title = book_data["title"], description = book_data["description"])
